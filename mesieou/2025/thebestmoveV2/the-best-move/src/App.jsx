8de842c3f674cd626550363863ed216b289d4e0f
Juan Bernal <mesieou@gmail.com>
2025-01-08 16:35:32 +1100

    nav bar

---
+++ mesieou/2025/thebestmoveV2/the-best-move/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="container">
+        <nav>
+          <Link to="/">
+            <img
+              src="/src/assets/Marketing/logo/Marketing/logo/white Logo and title v2 (2).svg"
+              alt=""
+              className="logo"
+            />
+          </Link>
+          <ul className="nav-links">
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Pricing</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+          <Link to="/">
+            <button>Quote Now</button>
+          </Link>
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default App;
